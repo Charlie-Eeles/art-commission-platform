e0ee82eb99f5b2cca1cb1aab207cff1c0fbc38d3
Charlie Eeles <charlie.eeles@gmail.com>
2026-06-16 20:15:50 +0100

    feat: Add auth sub to users table

--- backend/db/migrations/20260527194604_add_accounts_schema_and_tables.sql
+++ backend/db/migrations/20260527194604_add_accounts_schema_and_tables.sql
@@ -4,6 +4,7 @@ CREATE SCHEMA IF NOT EXISTS accounts;
 CREATE TABLE accounts.users (
     id uuid PRIMARY KEY DEFAULT uuidv4() NOT NULL,
     email text NOT NULL,
+    auth_sub text NOT NULL,
     created_at TIMESTAMPTZ NOT NULL DEFAULT NOW(),
     updated_at TIMESTAMPTZ NOT NULL DEFAULT NOW()
 );
