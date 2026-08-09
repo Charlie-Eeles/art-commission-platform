40c2cbe99764cf9d1e0b6dd2fc9213c7acf2b140
Charlie Eeles <charlie.eeles@gmail.com>
2026-08-09 20:01:06 +0100

    feat: Add simple landing page

--- frontend/app/pages/index.vue
+++ frontend/app/pages/index.vue
@@ -1,3 +1,20 @@
 <template>
-  <div>Hello world, welcome to the Art Commission Platform!</div>
+  <main class="mt-28 flex justify-center px-6">
+    <div class="max-w-xl text-center">
+      <h1 class="text-4xl font-bold">
+        Welcome to the <br />
+        Art Commission Platform
+      </h1>
+      <p class="mt-4 mb-6 text-lg text-gray-600">
+        Discover artists and bring your ideas to life.
+      </p>
+      <NuxtLink to="/explore">
+        <Button label="Start exploring">
+          <template #icon>
+            <Icon name="material-symbols:arrow-forward" class="size-4" />
+          </template>
+        </Button>
+      </NuxtLink>
+    </div>
+  </main>
 </template>
