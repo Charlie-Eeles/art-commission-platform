8b48667787a298ea4e16c457ab1b111438a54308
Charlie Eeles <charlie.eeles@gmail.com>
2026-08-19 23:03:04 +0100

    feat: Simplify dockerfile and fix repo/host in deploy

--- scripts/bin/deploy.sh
+++ scripts/bin/deploy.sh
@@ -2,13 +2,15 @@
 
 set -e
 
-host="ubuntu@3.9.174.250"
+host="ubuntu@18.169.122.197"
 key=".ssh/art-commission-platform-api.pem"
-repo="git@github.com:charlie-eeles/art-commission-platform.git"
+repo="https://github.com/Charlie-Eeles/art-commission-platform.git"
 remote="/home/ubuntu/art-commission-platform"
 env_file="backend/.env.production"
 
 ssh -i "$key" "$host" "
+    set -e
+
     sudo apt-get update
     sudo apt-get install -y git docker.io docker-compose-v2
     sudo systemctl enable --now docker
@@ -28,6 +30,10 @@ scp -i "$key" \
     "$host:$remote/backend/.env"
 
 ssh -i "$key" "$host" "
+    set -e
+
+    chmod 600 '$remote/backend/.env'
+
     cd '$remote'
 
     sudo docker compose -f compose.production.yml up \
